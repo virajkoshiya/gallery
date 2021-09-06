8a264427e03f55a6a8998b170101ad01483b5333
viraj <virajkoshiya5995@gmail.com>
2021-09-06 14:02:27 +0530

    images (swipe to change images and give details )+video with button which play next video stop play restart function + brightness and + volume

--- app/src/main/java/com/example/aug_pick_image_video_share/play_video.java
+++ app/src/main/java/com/example/aug_pick_image_video_share/play_video.java
@@ -4,6 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
@@ -11,11 +15,13 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,60 +36,64 @@ import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class play_video extends AppCompatActivity {
-
-MediaController med;
-
-int pos;
-
+    private RelativeLayout mainvid;
+    private SeekBar seekvolume;
+    AudioManager audioManager;
+    private SeekBar seekbrightness;
+    MediaController med;
+    int pos;
     static int frd;
-
-    private TextView current,total;
-
+    private TextView current, total;
     private SeekBar seek;
-
-    private ImageView play,forwad,backward;
+    private ImageView play, forwad, backward;
     String pathsinglevideo;
-
     VideoView exoPlayerView;
-
     double current_pos, total_duration;
-
     private int videoTotalTime;
-
     ArrayList<V_sub_fold_facer> change;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video);
-        exoPlayerView=findViewById(R.id.video_view);
-     //   seek.setClickable(false);
-        forwad=(ImageView)findViewById(R.id.forwad);
-        backward=(ImageView)findViewById(R.id.backward);
 
-       current=(TextView) findViewById(R.id.current);
-        total=(TextView) findViewById(R.id.total);
+        mainvid = (RelativeLayout) findViewById(R.id.mainvid);
+
+        exoPlayerView = findViewById(R.id.video_view);
+        //   seek.setClickable(false);
+        forwad = (ImageView) findViewById(R.id.forwad);
+        backward = (ImageView) findViewById(R.id.backward);
+
+        seekvolume = (SeekBar) findViewById(R.id.seekvolume);
+
+        seekbrightness = (SeekBar) findViewById(R.id.seekbrightness);
+
+        current = (TextView) findViewById(R.id.current);
+        total = (TextView) findViewById(R.id.total);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxvol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentvol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
 
         // v_sub_fold_adapter mathi lidheli tya static declare kreli che
-             change= v_sub_fold_adapter.sub_videos;
+        change = v_sub_fold_adapter.sub_videos;
 
-            String s = change.get(0).getV_s_f_Path();
-        Log.e("pathrrr", "onCreate: "+ s);
+        String s = change.get(0).getV_s_f_Path();
+        Log.e("pathrrr", "onCreate: " + s);
 
 
-
-        med=new MediaController(this);
+        med = new MediaController(this);
 
         // intent come from v_sub_fold_adapter
 
-         pathsinglevideo=  getIntent().getStringExtra("play");
-            pos= getIntent().getIntExtra("position",0);
-            // old method
-      //  exoPlayerView.setVideoPath(pathsinglevideo);
-     //   exoPlayerView.setMediaController(med);
-      //  med.setAnchorView(exoPlayerView);
-      //  exoPlayerView.start();
+        pathsinglevideo = getIntent().getStringExtra("play");
+        pos = getIntent().getIntExtra("position", 0);
+        // old method
+        //  exoPlayerView.setVideoPath(pathsinglevideo);
+        //   exoPlayerView.setMediaController(med);
+        //  med.setAnchorView(exoPlayerView);
+        //  exoPlayerView.start();
 
 
         seek = (SeekBar) findViewById(R.id.seek);
@@ -91,8 +101,7 @@ int pos;
         play = (ImageView) findViewById(R.id.play);
 
 
-
-           PlayVideo(pos);
+        PlayVideo(pos);
 
 
         forwad.setOnClickListener(new View.OnClickListener() {
@@ -125,16 +134,82 @@ int pos;
         });
 
 
-
-
-
-
         exoPlayerView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
 
-               // seek.setMax(exoPlayerView.getDuration());
+                // seek.setMax(exoPlayerView.getDuration());
                 setVideoProgress();
+
+            }
+        });
+
+        int brightness = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
+        seekbrightness.setProgress(brightness);
+
+        seekbrightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Context c = getApplicationContext();
+                boolean canwrite = Settings.System.canWrite(c);
+                if (canwrite) {
+                    int sbrightness = progress * 255 / 255;
+                    Settings.System.putInt(c.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.
+                            System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+                    Settings.System.putInt(c.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, sbrightness);
+
+                    //   Toast.makeText(c, "if", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                    startActivity(intent);
+                    // Toast.makeText(c, "else", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekvolume.setMax(maxvol);
+        seekvolume.setProgress(currentvol);
+        seekvolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mainvid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                 visiblity();
+
+                // Toast.makeText(v.getContext(), "Main layout", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -142,15 +217,28 @@ int pos;
 
     }
 
+    public void visiblity() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+
+
+
+
+            }
+        }, 3000);
+    }
+
 
     public void PlayVideo(int position) {
         try {
             exoPlayerView.setVideoURI(Uri.parse(change.get(position).getV_s_f_Path()));
-          //  title_txt.setText(list.get(position).getVideoTitle());
-          //  play_btn.setImageResource(R.drawable.pause_btn);
+
             exoPlayerView.requestFocus();
             exoPlayerView.start();
-             pos = position;
+            pos = position;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -200,7 +288,6 @@ int pos;
             public void onStartTrackingTouch(SeekBar seekBar) {
 
 
-
             }
 
             @Override
@@ -215,15 +302,10 @@ int pos;
     }
 
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void play(View view) {
 
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public void play(View view){
-
-        if (!exoPlayerView.isPlaying()){
+        if (!exoPlayerView.isPlaying()) {
             exoPlayerView.start();
             play.setImageResource(R.drawable.ply);
 
@@ -232,7 +314,7 @@ int pos;
                 @Override
                 public void onTick(long millisUntilFinished) {
 
-                    seek.setProgress(exoPlayerView.getCurrentPosition(),true);
+                    seek.setProgress(exoPlayerView.getCurrentPosition(), true);
                     seek.setProgress(exoPlayerView.getCurrentPosition());
 
                 }
@@ -243,8 +325,7 @@ int pos;
                 }
 
             };
-        }
-        else {
+        } else {
             exoPlayerView.pause();
             play.setImageResource(R.drawable.pause);
 
@@ -254,13 +335,13 @@ int pos;
 
     }
 
-    public void pause(View view){
+    public void pause(View view) {
 
         exoPlayerView.pause();
 
     }
 
-    public void restart(View view){
+    public void restart(View view) {
         exoPlayerView.stopPlayback();
         exoPlayerView.setVideoPath(pathsinglevideo);
         exoPlayerView.start();
